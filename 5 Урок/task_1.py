467a06e4cee093cae0edeb7e5f159bbdc28279d6
Oleg Koledov <koledov@bk.ru>
2020-05-07 20:23:05 +0700

    Задание на 5 урок

---
+++ 5 Урок/task_1.py
@@ -0,0 +1,91 @@
+"""
+1.	Пользователь вводит данные о количестве предприятий, их наименования и прибыль
+за 4 квартала (т.е. 4 отдельных числа) для каждого предприятия.
+Программа должна определить среднюю прибыль (за год для всех предприятий)
+и вывести наименования предприятий, чья прибыль выше среднего и отдельно
+вывести наименования предприятий, чья прибыль ниже среднего.
+
+Подсказка:
+Для решения задачи обязательно примените какую-нибудь коллекцию из модуля collections
+Для лучшее освоения материала можете даже сделать несколько решений этого задания,
+применив несколько коллекций из модуля collections
+
+Пример:
+Введите количество предприятий для расчета прибыли: 2
+Введите название предприятия: Рога
+через пробел введите прибыль данного предприятия
+за каждый квартал(Всего 4 квартала): 235 345634 55 235
+
+Введите название предприятия: Копыта
+через пробел введите прибыль данного предприятия
+за каждый квартал(Всего 4 квартала): 345 34 543 34
+
+Средняя годовая прибыль всех предприятий: 173557.5
+Предприятия, с прибылью выше среднего значения: Рога
+
+Предприятия, с прибылью ниже среднего значения: Копыта
+"""
+import collections
+import random
+
+
+def sum_tuple(numbers):
+    '''tuple --> sum'''
+
+    total_sum = 0
+    for sum_q in numbers:
+        total_sum += sum_q
+        return total_sum
+
+
+Enterprise = collections.namedtuple('Enterprise', ['q1', 'q2', 'q3', 'q4'])
+
+base_enterprise = {}
+
+n = int(input("Количество предприятий: "))
+
+for i in range(n):
+    name = input(str(i+1) + '-е предприятие: ')
+    profit_q1 = int(input('Прибыль за 1-й квартал: '))
+    profit_q2 = int(input('Прибыль за 2-й квартал: '))
+    profit_q3 = int(input('Прибыль за 3-й квартал: '))
+    profit_q4 = int(input('Прибыль за 4-й квартал: '))
+    base_enterprise[name] = Enterprise(
+        q1=profit_q1,
+        q2=profit_q2,
+        q3=profit_q3,
+        q4=profit_q4
+    )
+
+base_enterprise['Name1'] = Enterprise(
+    q1=random.randint(100, 500),
+    q2=random.randint(100, 500),
+    q3=random.randint(100, 500),
+    q4=random.randint(100, 500)
+)
+
+base_enterprise['Name2'] = Enterprise(
+    q1=random.randint(100, 500),
+    q2=random.randint(100, 500),
+    q3=random.randint(100, 500),
+    q4=random.randint(100, 500)
+)
+
+total_profit = ()
+
+for name, profit in base_enterprise.items():
+    print(f'Предприятие: {name} прибыль за год - {sum(profit)}')
+    total_profit += profit
+
+avg_profit_total = sum(total_profit) / len(base_enterprise)
+print(f'Средняя прибыль за год для всех предприятий {avg_profit_total}')
+
+print('Предприятия, у которых прибыль выше среднего:')
+
+for name, profit in base_enterprise.items():
+    if sum(profit) > avg_profit_total:
+        print(f'{name} - {sum(profit)}')
+
+for name, profit in base_enterprise.items():
+    if sum(profit) < avg_profit_total:
+        print(f'{name} - {sum(profit)}')
